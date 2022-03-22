fb0abe81ab14951ae1b2cdb35f6822379baed572
JoyH127 <hh495@scarletmail.rutgers.edu>
2022-03-21 21:58:35 -0400

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+export const items = [
+  {
+    title: "Breaded Mozzarella Cheese Stick",
+    brand: "FarmRich",
+    units: "45 pieces",
+    quantity: 50,
+    isPurchased: true,
+  },
+  {
+    title: "OREO Double Stuf Chocolate SandWich Cookies",
+    brand: "Oreo",
+    units: "30 pieces",
+    quantity: 50,
+    isPurchased: true,
+  },
+  {
+    title: "Jimmy Dean Original Pancakes& Sausage on a Stick",
+    brand: "Oreo",
+    units: "12 sticks",
+    quantity: 50,
+    isPurchased: true,
+  },
+];
+
+export default { items };
